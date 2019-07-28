9cbc7e8757255df123f8399cd0b396394f7da9e8
Junior Asmat <45374402+juniotasmat22@users.noreply.github.com>
2019-07-28 01:31:51 -0500

    entidades

---
+++ entidades/Proceso.php
@@ -0,0 +1,39 @@
+<?php
+namespace entidades;
+use PDO;
+
+class Proceso extends Entidad
+{
+  private $idProceso;
+  private $idMapaProcesos;
+  private $tipo;
+  private $nombre;
+  private $descripcion;
+  private $estado;
+  public function setConsulta($filaConsulta){
+    $this->idProceso=$this->obtenerColumna($filaConsulta,'idProceso');
+    $this->idMapaProcesos=$this->obtenerColumna($filaConsulta,'idMapaProcesos');
+    $this->tipo=$this->obtenerColumna($filaConsulta,'tipo');
+    $this->nombre=$this->obtenerColumna($filaConsulta,'nombre');
+    $this->descripcion=$this->obtenerColumna($filaConsulta,'descripcion');
+    $this->estado=$this->obtenerColumna($filaConsulta,'estado');
+  }
+  public function bindValues($statement){
+    $statement->bindValue(1,$this->idProceso,PDO::PARAM_INT);
+    $statement->bindValue(2,$this->idMapaProcesos,PDO::PARAM_INT);
+    $statement->bindValue(3,$this->tipo,PDO::PARAM_STR);
+    $statement->bindValue(4,$this->nombre,PDO::PARAM_STR);
+    $statement->bindValue(5,$this->descripcion,PDO::PARAM_STR);
+    $statement->bindValue(6,$this->estado,PDO::PARAM_INT);
+    return $statement;
+  }
+
+  public function set($metodo){
+    $this->idProceso 	       = $metodo('idProceso ');
+    $this->idMapaProcesos	   = $metodo('idMapaProcesos');
+    $this->tipo	             = $metodo('tipo');
+    $this->nombre            = $metodo('nombre');
+    $this->descripcion	     = $metodo('descripcion');
+    $this->estado 	         = $metodo('estado');
+  }
+}
---
+++ entidades/Subproceso.php
@@ -0,0 +1,40 @@
+<?php
+namespace entidades;
+use PDO;
+
+class Subproceso extends Entidad
+{
+  private $idSubproceso;
+  private $idProceso;
+  private $nombre;
+  private $descripcion;
+  private $estado;
+  private $idSubPro;
+  public function setConsulta($filaConsulta){
+    $this->idSubproceso=$this->obtenerColumna($filaConsulta,'idSubproceso');
+    $this->idProceso=$this->obtenerColumna($filaConsulta,'idProceso');
+    $this->nombre=$this->obtenerColumna($filaConsulta,'nombre');
+    $this->descripcion=$this->obtenerColumna($filaConsulta,'descripcion');
+    $this->estado=$this->obtenerColumna($filaConsulta,'estado');
+    $this->idSubPro=$this->obtenerColumna($filaConsulta,'idSubPro');
+  }
+  public function bindValues($statement){
+    $statement->bindValue(1,$this->idSubproceso,PDO::PARAM_INT);
+    $statement->bindValue(2,$this->idProceso,PDO::PARAM_INT);
+    $statement->bindValue(3,$this->nombre,PDO::PARAM_STR);
+    $statement->bindValue(4,$this->descripcion,PDO::PARAM_STR);
+    $statement->bindValue(5,$this->estado,PDO::PARAM_INT);
+    $statement->bindValue(6,$this->idSubPro,PDO::PARAM_STR);
+    return $statement;
+  }
+
+  public function set($metodo){
+    $this->idSubproceso 	   = $metodo('idSubproceso ');
+    $this->idProceso	       = $metodo('idProceso');
+    $this->idSubPro	         = $metodo('idSubPro');
+    $this->nombre            = $metodo('nombre');
+    $this->descripcion	     = $metodo('descripcion');
+    $this->estado 	         = $metodo('estado');
+  }
+
+}
